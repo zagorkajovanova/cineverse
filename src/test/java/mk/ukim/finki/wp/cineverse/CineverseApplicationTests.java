e6415d05be46852fe6885560792470e7300a6e24
Zagorka <jovanovazagorka@outlook.com>
2022-02-19 12:41:56 +0100

    Initial commit

---
+++ src/test/java/mk/ukim/finki/wp/cineverse/CineverseApplicationTests.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.wp.cineverse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CineverseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
